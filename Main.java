e7808843ae4a1eda2c40310dd100ab4529b655fe
Ranveersingh1113 <161949661+Ranveersingh1113@users.noreply.github.com>
2025-01-22 23:06:08 +0530

    uploaded Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+//Ranveer Singh 23070126102 AIML B1
+
+
+class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println("Result is: " + cal.calculate());
+    }
+}
